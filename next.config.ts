3f673e3238a57fde46a9c5f4fa7c16c83442ffcc
DrewAMSD <drewalgismiller2@gmail.com>
2026-04-05 23:49:23 -0700

    simple light them styling

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // comment this out if you want the dev indicator
+  devIndicators: false
 };
 
 export default nextConfig;
