52f9c8788663e3e3d2d1e84b0e81c0d6b205de76
DrewAMSD <drewalgismiller2@gmail.com>
2026-04-12 23:44:10 -0700

    small changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,10 @@ export default function Navbar() {
     const anchorClass: string = "text-lg";
 
     return (
-        <div className="flex flex-row items-center justify-between w-full h-[8vh] min-h-20">
-            <p className="w-[60vw] text-left text-2xl pl-[2vw] lg:pl-[4vw]">Drew Miller</p>
+        <div className="flex flex-row items-center justify-between w-full h-[8vh] min-h-20 pl-[2vw] lg:pl-[4vw]">
+            <p className="w-[60vw] text-left text-2xl">Drew Miller</p>
             <nav className="w-[32vw] flex flex-row items-center justify-evenly w-full">
-                {/* <a href="#hero">Hero</a> */}
+                {/* <a href="#hero">Home</a> */}
                 <a href="#about" className={anchorClass}>About</a>
                 <a href="#projects" className={anchorClass}>Projects</a>
                 <a href="#contact" className={anchorClass}>Contact</a>
